17939446d29eda168382d24605509340d5a8cb43
penguin02007 <penguin02007@gmail.com>
2026-05-25 21:51:39 -0400

    all fixed

--- l3vpn/2.1-basic/sanitize_junos_config.sh
+++ l3vpn/2.1-basic/sanitize_junos_config.sh
@@ -5,8 +5,8 @@ main() {
     if [ -z "$1" ]; then
         get_help
     else
-        echo "Processing: $1"
-        for i in `1 2 5 6`; do sshpass -padmin@123 ssh clab-ml_$1-vr$i "\
+        echo "Processing: clab multi-lab $1"
+        for i in 1 2 5 6; do sshpass -padmin@123 ssh clab-ml_$1-vr$i "\
         edit;\
         delete interfaces ge-0/0/0; \
         delete interfaces ge-0/0/1; \
