deff52f011146c24dc9738d4c1374669fcbeff7d
penguin02007 <penguin02007@gmail.com>
2026-05-25 21:46:20 -0400

    make it a function

--- l3vpn/2.1-basic/sanitize_junos_config.sh
+++ l3vpn/2.1-basic/sanitize_junos_config.sh
@@ -1,8 +1,22 @@
 # /bin/bash
 
-for i in `1 2 5 6`; do sshpass -padmin@123 ssh clab-ml_$1-vr$i "\
- edit;\
- delete interfaces ge-0/0/0; \
- delete interfaces ge-0/0/1; \
- commit and-quit"; \
- done
+main() {
+    for i in `1 2 5 6`; do sshpass -padmin@123 ssh clab-ml_$1-vr$i "\
+    edit;\
+    delete interfaces ge-0/0/0; \
+    delete interfaces ge-0/0/1; \
+    commit and-quit"; \
+    done
+
+    if [ -z "$1" ]; then
+        display_help
+    else
+        echo "Processing: $1"
+    fi
+}
+
+help() {
+    echo "Usage: $0 [multi-lab number]"
+    echo "Example: $0 21"
+}
+main "$1"
