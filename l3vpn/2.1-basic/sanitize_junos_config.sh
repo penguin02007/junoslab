d3aca1af959eda0d580d23de6afe89c5bc6d1211
penguin02007 <penguin02007@gmail.com>
2026-05-30 13:16:28 -0400

    delete unit 0

--- l3vpn/2.1-basic/sanitize_junos_config.sh
+++ l3vpn/2.1-basic/sanitize_junos_config.sh
@@ -4,11 +4,19 @@ main() {
     if [ -z "$1" ]; then
         get_help
     else
-        echo "Processing: clab multi-lab $1"
+        echo "Delete Interfaces for LACP: clab multi-lab $1"
         for i in 1 2 5 6; do sshpass -padmin@123 ssh clab-ml_$1-vr$i "\
         edit;\
         delete interfaces ge-0/0/0; \
         delete interfaces ge-0/0/1; \
+        commit and-quit"; \
+        done
+        echo "Clean Up Unit 0 in Interfaces: clab multi-lab $1"
+        for i in 1 2 3 4 5 6 7 8; do sshpass -padmin@123 ssh clab-ml_$1-vr$i "\
+        edit;\
+        delete interfaces ge-0/0/2.0; \
+        delete interfaces ge-0/0/3.0; \
+        delete interfaces ge-0/0/4.0; \
         commit and-quit"; \
         done
     fi
