bcbfa1f4875214611b1de30a12d1cbed0e328f41
Leo Chan <penguin02007@gmail.com>
2026-06-12 18:41:43 +0000

    option a

--- get_junos_config.sh
+++ get_junos_config.sh
@@ -1,6 +1,7 @@
 for i in {1..8}; do
-    ml="3"
+    ml="$1"
+    state="$2"
     vmx="$i"
     sshpass -p 'admin@123' scp "admin@clab-ml_$ml-vr$vmx:/config/juniper.conf.gz" "vr$vmx.xml.conf.gz"
-    gunzip -c "vr$vmx.xml.conf.gz" > "configs/vr$vmx.conf"
+    gunzip -c "vr$vmx.xml.conf.gz" > "vr$vmx-$state.conf"
 done
