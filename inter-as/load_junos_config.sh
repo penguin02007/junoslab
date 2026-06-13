ac3ec034906d35a19121130c834d8d7baf8f82a8
penguin02007 <penguin02007@gmail.com>
2026-06-13 11:26:40 -0400

    fix ce devices

--- inter-as/load_junos_config.sh
+++ inter-as/load_junos_config.sh
@@ -7,7 +7,7 @@ main() {
         for i in 1 2 3 4 5 6 7 8; do sshpass -padmin@123 scp -o "StrictHostKeyChecking=no" vr$i-start.conf admin@clab-ml_$1-vr$i:/var/tmp/;done
         for i in 1 2 3 4 5 6 7 8; do sshpass -padmin@123 ssh admin@clab-ml_$1-vr$i "edit;load merge /var/tmp/vr$i-start.conf;commit and-quit";done
         for i in vce1-1 vce1-2 vce1-3 vce1-4 vce2-1 vce2-2 vce2-3 vce2-4 vce2-5 vce3-1 vce3-2; do sshpass -padmin@123 scp -o "StrictHostKeyChecking=no" vr$i-start.conf admin@clab-ml_$1-vr$i:/var/tmp/;done
-        for i in vce1-1 vce1-2 vce1-3 vce1-4 vce2-1 vce2-2 vce2-3 vce2-4 vce2-5 vce3-1 vce3-2; do sshpass -padmin@123 ssh admin@clab-ml_$1-vr$i "edit;load merge /var/tmp/vr$i-start.conf;commit and-quit";done
+        for i in vce1-1 vce1-2 vce1-3 vce1-4 vce2-1 vce2-2 vce2-3 vce2-4 vce2-5 vce3-1 vce3-2; do sshpass -padmin@123 ssh admin@clab-ml_$1-$i "edit;load merge /var/tmp/vr$i-start.conf;commit and-quit";done
     fi
 }
 get_help() {
