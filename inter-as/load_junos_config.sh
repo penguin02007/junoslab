56e2105596c774f5caca06748e192095e67327af
penguin02007 <penguin02007@gmail.com>
2026-06-15 20:51:36 -0400

    update load script

--- inter-as/load_junos_config.sh
+++ inter-as/load_junos_config.sh
@@ -1,5 +1,8 @@
 # /bin/bash
 
+ml="$1"
+state="$2"
+
 get_help() {
     echo "Usage: $0 [multi-lab number]"
     echo "Example: $0 21"
@@ -32,8 +35,8 @@ main() {
     else
         for i in {1..8} 10; do sshpass -padmin@123 scp -o "StrictHostKeyChecking=no" vr$i-start.conf admin@clab-ml-$1-vr$i:/var/tmp/;done
         for i in {1..8} 10; do sshpass -padmin@123 ssh admin@clab-ml-$1-vr$i "edit;load merge /var/tmp/vr$i-start.conf;commit and-quit";done
-        for i in vce1-1 vce1-2 vce1-3 vce1-4 vce2-1 vce2-2 vce2-3 vce2-4 vce2-5 vce3-1 vce3-2; do sshpass -padmin@123 scp -o "StrictHostKeyChecking=no" $i-start.conf admin@clab-ml-$1-$i:/var/tmp/;done
-        for i in vce1-1 vce1-2 vce1-3 vce1-4 vce2-1 vce2-2 vce2-3 vce2-4 vce2-5 vce3-1 vce3-2; do sshpass -padmin@123 ssh admin@clab-ml-$1-$i "edit;load merge /var/tmp/$i-start.conf;commit and-quit";done
+        for i in vce1-1 vce1-2 vce1-3 vce1-4 vce2-1 vce2-2 vce2-3 vce2-4 vce2-5 vce3-1 vce3-2; do sshpass -padmin@123 scp -o "StrictHostKeyChecking=no" $i-$state.conf admin@clab-ml-$1-$i:/var/tmp/;done
+        for i in vce1-1 vce1-2 vce1-3 vce1-4 vce2-1 vce2-2 vce2-3 vce2-4 vce2-5 vce3-1 vce3-2; do sshpass -padmin@123 ssh admin@clab-ml-$1-$i "edit;load merge /var/tmp/$i-$state.conf;commit and-quit";done
     fi
 }
 get_help() {
