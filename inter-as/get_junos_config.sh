8ae3e04646c99dd87fc82b1976432c9efabfd1a3
penguin02007 <penguin02007@gmail.com>
2026-06-19 13:20:28 -0400

    evpn config

--- inter-as/get_junos_config.sh
+++ inter-as/get_junos_config.sh
@@ -23,7 +23,7 @@ else
         rm "vr$vmx.xml.conf.gz"
     done
 
-    for vmx in vce1-1 vce1-2 vce1-3 vce1-4 vce2-1 vce2-2 vce2-3 vce2-4 vce2-5 vce3-1 vce3-2; do
+    for vmx in vdc2 vce1-1 vce1-2 vce1-3 vce1-4 vce2-1 vce2-2 vce2-3 vce2-4 vce2-5 vce3-1 vce3-2; do
         echo "Downloading configuration for $vmx..."
         
         # Using sshpass to pull the configuration
