5aed5e849fcf739e4f5732dfe98a3652e369a1b1
penguin02007 <penguin02007@gmail.com>
2026-06-20 16:50:05 -0400

    update get_junos_config

--- inter-as/get_junos_config.sh
+++ inter-as/get_junos_config.sh
@@ -15,22 +15,22 @@ else
         vmx="$i"
         echo "Downloading configuration for vMX $vmx..."
         
-        # Using sshpass to pull the configuration
         sshpass -p 'admin@123' scp "admin@clab-ml-$ml-vr$vmx:/config/juniper.conf.gz" "vr$vmx.xml.conf.gz"
-        
-        # Decompressing and clean up
+
+        # pull config and clean up
         gunzip -c "vr$vmx.xml.conf.gz" > "vr$vmx-$state.conf"
+        sed -i '/Last changed/d' "$vmx-$state.conf"
         rm "vr$vmx.xml.conf.gz"
     done
 
     for vmx in vdc2 vce1-1 vce1-2 vce1-3 vce1-4 vce2-1 vce2-2 vce2-3 vce2-4 vce2-5 vce3-1 vce3-2; do
         echo "Downloading configuration for $vmx..."
         
-        # Using sshpass to pull the configuration
         sshpass -p 'admin@123' scp "admin@clab-ml-$ml-$vmx:/config/juniper.conf.gz" "$vmx.xml.conf.gz"
         
-        # Decompressing and clean up
+        # pull config and clean up
         gunzip -c "$vmx.xml.conf.gz" > "$vmx-$state.conf"
+        sed -i '/Last changed/d' "$vmx-$state.conf"
         rm "$vmx.xml.conf.gz"
     done
 fi
