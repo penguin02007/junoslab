c298ef2b0a0792f9a0e0b72a75bcebca7781d3af
Leo Chan <penguin02007@users.noreply.github.com>
2026-08-12 14:23:56 -0400

    update node names

--- l3vpn/l3vpn-basic/load_junos_config.sh
+++ l3vpn/l3vpn-basic/load_junos_config.sh
@@ -4,8 +4,8 @@ main() {
     if [ -z "$1" ]; then
         get_help
     else
-        for i in 1 2 3 4 5 6 7 8 10; do sshpass -padmin@123 scp -o "StrictHostKeyChecking=no" vr$i-start.conf admin@clab-ml_$1-vr$i:/var/tmp/;done
-        for i in 1 2 3 4 5 6 7 8 10; do sshpass -padmin@123 ssh admin@clab-ml_$1-vr$i "edit;load merge /var/tmp/vr$i-start.conf;commit and-quit";done
+        for i in 1 2 3 4 5 6 7 8 10; do sshpass -padmin@123 scp -o "StrictHostKeyChecking=no" vr$i-start.conf admin@clab-ml-$1-vr$i:/var/tmp/;done
+        for i in 1 2 3 4 5 6 7 8 10; do sshpass -padmin@123 ssh admin@clab-ml-$1-vr$i "edit;load merge /var/tmp/vr$i-start.conf;commit and-quit";done
     fi
 }
 get_help() {
